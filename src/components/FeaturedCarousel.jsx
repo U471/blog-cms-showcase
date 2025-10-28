1725fd560a1cb31ebce6031a2a1c446c8c8f114b
Muhammad Usama Tahir <usamatahir8600@gmail.com>
2025-10-29 00:07:33 +0500

    update

--- src/components/FeaturedCarousel.jsx
+++ src/components/FeaturedCarousel.jsx
@@ -7,7 +7,7 @@ import 'slick-carousel/slick/slick-theme.css'
 export default function FeaturedCarousel({items}){
   const settings = {
     dots: true,
-    infinite: true,
+    infinite: items.length > 3,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
